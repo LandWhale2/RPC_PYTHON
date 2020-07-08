138768b9ed92835e2addc6cc1398207e455c3003
kcuyh <khj879123@gmail.com>
2020-07-08 12:42:20 +0900

    server update

--- rpc_server.py
+++ rpc_server.py
@@ -1,16 +1,18 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
-import os
+
 
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
-
-with SimpleXMLRPCServer(('localhost',8000),
+with SimpleXMLRPCServer(('0.0.0.0',9000),
                         requestHandler=RequestHandler)as server:
     server.register_introspection_functions()
 
+    def message(request):
+        print("response {}".format(request))
+        return "response {}".format(request)
 
-
+    server.register_function(message,'message')
     server.serve_forever()
